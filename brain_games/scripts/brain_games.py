7b8b2c6879a7e2cc6807864bc6b73a4f3d6e143a
usman sakhbeev <sahbeyev@gmail.com>
2024-04-29 10:05:38 +0300

    updated brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
-sys.path.append('/home/usman/python-project-49/brain_games')
+sys.path.append('home/usman/python-project-49/brain_games')
 
 from cli import welcome_user
-from brain_even import even_game
 
 
 def main():
 	welcome_user()
-	even_game()
 
 
 if __name__ == '__main__':
