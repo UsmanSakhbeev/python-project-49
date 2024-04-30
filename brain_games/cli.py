d7dab969e645a9aa9c5b386ad3910e2ee72f2368
usman sakhbeev <sahbeyev@gmail.com>
2024-04-30 19:14:37 +0300

    updated cli.py and script_brain_games

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,15 +1,6 @@
 import prompt
 
 def welcome_user():
-	print("Welcome to the Brain Games!")
 	name = prompt.string("May i have your name? ")
 	print(f"Hello, {name}!")
-	return name
-
-
-def main():
-	welcome_user()
-
-
-if __name__ == '__main__':
-	main()
+	
