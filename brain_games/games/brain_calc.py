1242eea3f8e234b8f84d68055410b13c18fdb9e2
usman sakhbeev <sahbeyev@gmail.com>
2024-05-01 21:56:02 +0300

    updated calc game

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,55 +1,23 @@
+#!/usr/bin/env python3
 
-import prompt
 import random
-import sys
-sys.path.append('/home/usman/python-project-49/brain_games')
 
-from brain_games.cli import welcome_user
 
-name = "Dyadya Bob"
-
+game_rule = "What is the result of the expression?"
 
 def calc_game():
-    first_number = 0
-    second_number = 0
-    operation = 0
-    corrects_in_row = 0
-    correct_answer = 0
-    answer = '0'
-    print("What is result of the expresion?")
-
-    while corrects_in_row < 3:
-        first_number = random.randint(2, 100)
-        second_number = random.randint(2, 100)
-        operation = random.randint(1, 3)
-        if operation == 1:
-            correct_answer = first_number + second_number
-            print(f"Question: {first_number} + {second_number}")
-        elif operation == 2:
-            correct_answer = first_number - second_number
-            print(f"Question: {first_number} - {second_number}")
-        else:
-            correct_answer = first_number * second_number
-            print(f"Question: {first_number} * {second_number}")
-        answer = prompt.string("Your answer: ")
-        if answer == str(correct_answer):
-            print("Correct!")
-            corrects_in_row += 1
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            break
-
-    if corrects_in_row == 3:
-        print(f"Congratulations, {name}")
+    first_number = random.randint(2, 100)
+    second_number = random.randint(2, 100)
+    operation = random.randint(1, 3)
+    if operation == 1:
+        correct_answer = first_number + second_number
+        question = f"{first_number} + {second_number}"
+    elif operation == 2:
+        correct_answer = first_number - second_number
+        question = f"{first_number} - {second_number}"
     else:
-        print(f"Let's try again, {name}")
+        correct_answer = first_number * second_number
+        question = f"{first_number} * {second_number}"
+    return (question, correct_answer)
 
 
-def main():
-    global name
-    name = welcome_user()
-    calc_game()
-
-
-if __name__ == "__main__":
-    main()
---
+++ brain_games/scripts/brain_calc_script.py
@@ -0,0 +1,10 @@
+from brain_games.engine import play_game
+from brain_games.games.brain_calc import calc_game, game_rule
+
+
+def main():
+    play_game(calc_game, game_rule)
+
+
+if __name__ == '__main__':
+    main()
