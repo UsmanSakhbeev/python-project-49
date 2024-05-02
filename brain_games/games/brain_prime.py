39a3e919d4681ad38e4fdfe0a693fa87198410c9
usman sakhbeev <sahbeyev@gmail.com>
2024-05-02 11:18:42 +0300

    added prime game

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,52 +1,18 @@
-import prompt
 import random
-import sys
-sys.path.append('/home/usman/python-project-49/brain_games')
 
-from brain_games.cli import welcome_user
+game_rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
-name = "Dyadya Bob"
+def prime_game():
+    number = random.randint(2, 200)
+    question = number
 
-
-def is_prime_game():
-    number = 0
-    corrects_in_row = 0
-    correct_answer = "False"
-    answer = '0'
-    print("Answer 'yes' if given number is prime. Otherwise answer 'no'.")
-
-    while corrects_in_row < 3:
-        number = random.randint(2, 200)
-        print(f"Question: {number}")
-
-        divider = 2
-        while number % divider != 0:
-            divider += 1
-        if divider == number:
-            correct_answer = "yes"
-        else:
-            correct_answer = "no"
-
-        answer = prompt.string("Your answer: ")
-
-        if str(correct_answer) == answer:
-            corrects_in_row += 1
-            print("Correct!")
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            break
-
-    if corrects_in_row == 3:
-        print(f"Congratulations, {name}")
+    divider = 2
+    while number % divider != 0:
+        divider += 1
+    if divider == number:
+        correct_answer = "yes"
     else:
-        print(f"Let's try again, {name}")
+        correct_answer = "no"
+    return(question, correct_answer)
 
 
-def main():
-    global name
-    name = welcome_user()
-    is_prime_game()
-
-
-if __name__ == "__main__":
-    main()
---
+++ brain_games/scripts/brain_prime_script.py
@@ -0,0 +1,10 @@
+from brain_games.engine import play_game
+from brain_games.games.brain_prime import prime_game, game_rule
+
+
+def main():
+    play_game(prime_game, game_rule)
+
+
+if __name__ == '__main__':
+    main()
