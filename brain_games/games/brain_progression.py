fcc6fe7029a9ae360d48f9ff00a7ee2676b5653f
usman sakhbeev <sahbeyev@gmail.com>
2024-05-05 09:54:21 +0300

    updated brain-progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -4,11 +4,11 @@ import random
 game_rule = "What number is missing in the progression?"
 
 
-def create_progression(current_number, step_of_progression, length_of_progression):
-    return [current_number + i*step_of_progression for i in range(length_of_progression)]
+def create_progression(current_number, step, length):
+    return [current_number + i * step for i in range(length)]
 
 
-def remove_el_from_progression(list, missing_element):    
+def remove_el_from_progression(list, missing_element):
     string = ""
     for index, value in enumerate(list):
         if index != missing_element:
@@ -18,16 +18,15 @@ def remove_el_from_progression(list, missing_element):
     return string
 
 
-
 def progression_game():
     current_number = random.randint(2, 100)
-    length_of_progression = random.randint(5, 10)
-    step_of_progression = random.randint(2, 10)
-    missing_element = random.randint(2, length_of_progression-1)
+    length = random.randint(5, 10)
+    step = random.randint(2, 10)
+    missing_element = random.randint(2, length - 1)
 
-    progression = create_progression(current_number, step_of_progression, length_of_progression)
+    progression = create_progression(current_number, step, length)
     question = remove_el_from_progression(progression, missing_element)
 
-    correct_answer = current_number + missing_element * step_of_progression
+    correct_answer = current_number + missing_element * step
 
     return (question, correct_answer)
