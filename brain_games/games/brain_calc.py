ab754d7b2d3b95f5018eb4c1facd0d38dfa7e57f
usman sakhbeev <sahbeyev@gmail.com>
2024-05-30 18:05:53 +0300

    modified brain_calc

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,20 +1,24 @@
 import random
+import operator
 
 
 game_rule = "What is the result of the expression?"
 
 
+def get_operator_func(op):
+    operators = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul
+    }
+    return operators[op]
+
+
 def calc_game():
     first_number = random.randint(2, 100)
     second_number = random.randint(2, 100)
-    operation = random.randint(1, 3)
-    if operation == 1:
-        correct_answer = first_number + second_number
-        question = f"{first_number} + {second_number}"
-    elif operation == 2:
-        correct_answer = first_number - second_number
-        question = f"{first_number} - {second_number}"
-    else:
-        correct_answer = first_number * second_number
-        question = f"{first_number} * {second_number}"
+    operation = random.choice(['+', '-', '*'])
+    operator_func = get_operator_func(operation)
+    correct_answer = operator_func(first_number, second_number)
+    question = f"{first_number} {operation} {second_number}"
     return (question, correct_answer)
