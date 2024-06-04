c3dd51fdbf50a78ab3fc3ec4348477cda82f917d
UsmanSakhbeev <sahbeyev@gmail.com>
2024-06-04 14:36:53 +0300

    updade engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,7 +1,7 @@
 import prompt
 
 
-round_counts = 3
+Round_counts = 3
 
 
 def play_game(get_question_answer, game_rules):
@@ -9,7 +9,7 @@ def play_game(get_question_answer, game_rules):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!\n{game_rules}')
 
-    for _ in range(round_counts):
+    for _ in range(Round_counts):
         question, correct_answer = get_question_answer()
         print(f'Question: {question}')
         player_answer = prompt.string('Your answer: ')
